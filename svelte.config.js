b479138eda69f349d6f2143047827b64aa2055c5
chenxiang <chenxiang@cmss.chinamobile.com>
2025-12-09 16:52:33 +0800

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
       precompress: false,
-      strict: true
-    })
-  }
+      strict: true,
+    }),
+  },
+  paths: {
+    base: process.env.NODE_ENV === "production" ? "./" : "",
+  },
 };
 
 export default config;
