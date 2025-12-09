a0cfb3f5e785ab0b23100509f9eca16f07552117
chenxiang <chenxiang@cmss.chinamobile.com>
2025-12-09 17:03:02 +0800

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "./" : "aaa",
+      base: process.env.NODE_ENV === "production" ? "/test" : "",
     },
   },
 };
